4c58efa1f31a57880ae4e6f8da48778c39134866
Rakibul Islam <rakibul.islam7772588@gmail.com>
2019-12-21 15:38:13 +0600

    first commit

---
+++ djblog/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'blog/about.html', context)
